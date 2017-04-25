453cdfc16861122f085a00b470f14ca82452550c
Justin Taylor <pianojet@gmail.com>
2017-04-25 19:20:40 -0400

    better audio file selecting

--- tools_audio_player_api/gui_ns_fcn/loadAudio.m
+++ tools_audio_player_api/gui_ns_fcn/loadAudio.m
@@ -1,11 +1,14 @@
 function loadAudio(handles)
+  conf = getappdata(0, 'conf');
   [filename, pathname] = uigetfile('*.wav', 'select an WAV file');
   %dataPath = '/Users/justin/Documents/MATLAB/medsim/data/med4_mashup';
+  fullpath = [pathname, filename];
+  disp('fullpath:');
+  disp(fullpath);
 
-  disp('[pathname, filename]:');
-  disp([pathname, filename]);
-
-  set(handles.dataPath, 'String', [pathname, filename]);
+  set(handles.dataPath, 'String', fullpath);
+  conf.audioFile = fullpath;
+  setappdata(0, 'conf', conf);
   % % conf = resetConfig();
   % conf.audioFile = [pathname, filename];
   initializePlayback(handles); % getPlaybackHandles
